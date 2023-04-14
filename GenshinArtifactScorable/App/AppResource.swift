ed4fc7f834c2f63ccda2a30d8ccf4b7066cb6721
hide9201 <shirasu_440037@outlook.jp>
2023-04-14 22:18:27 +0900

    [Delete] 使用しなくなった変数，構造体を削除

--- GenshinArtifactScorable/App/AppResource.swift
+++ GenshinArtifactScorable/App/AppResource.swift
@@ -11,38 +11,13 @@ final class AppResource {
     
     static let shared = AppResource()
     
-    let characterNameFromAvatarIdJSON: JSON?
-    let namecardURLFromNamecardIdJSON: JSON?
     let characterDetails: [String: CharacterMap.Character]?
     let nameCard: [String: NameCardMap.NameCard]?
     let localizedDictionary: [String: String]?
     
     private init() {
-        let jsonReader = JSONFileReader()
-
-        characterNameFromAvatarIdJSON = jsonReader.readJSONFile(filename: "charactersTemp")
-        namecardURLFromNamecardIdJSON = jsonReader.readJSONFile(filename: "namecards")
-        
         characterDetails = JSONFileDecoder<CharacterMap>.decode(fileName: "characters")?.characterStaticData
         nameCard = JSONFileDecoder<NameCardMap>.decode(fileName: "namecards")?.nameCard
         localizedDictionary = JSONFileDecoder<Localized>.decode(fileName: "loc")?.ja.content
-    }
-}
-
-struct JSONFileReader {
-    func readJSONFile(filename: String) -> JSON? {
-        guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else {
-            fatalError("ファイルが見つからない")
-        }
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("ファイル読み込みエラー")
-        }
-        do {
-            let jsonObject = try JSON(data: data)
-            return jsonObject
-        } catch let error {
-            print(error)
-            return nil
-        }
     }
 }
