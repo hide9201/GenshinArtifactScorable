60597e06b9110e64a2c0eb7979f7ccc5c43c0128
hide9201 <shirasu_440037@outlook.jp>
2023-05-31 14:10:45 +0900

    [Add] ビルドカードに武器と聖遺物の画像を表示できるように

--- GenshinArtifactScorable/Presentation/Screen/BuildCardGenerator/BuildCardGenerator.swift
+++ GenshinArtifactScorable/Presentation/Screen/BuildCardGenerator/BuildCardGenerator.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct BuildCardGenerator {
     
-    func buildCardCreate(buildCardBaseImage: UIImage, characterImage: UIImage) -> UIImage {
+    func buildCardCreate(buildCardBaseImage: UIImage, characterImage: UIImage, weaponImage: UIImage, artifactImages: [UIImage?]) -> UIImage {
         //画像の下地をサイズを指定して作成
         UIGraphicsBeginImageContextWithOptions(CGSize(width: 1920, height: 1080), false, 0)
         
@@ -22,8 +22,30 @@ struct BuildCardGenerator {
         let characterImageScale = characterImageMaxHeight / characterImage.size.height
         let characterImageWidth = characterImage.size.width * characterImageScale
         let characterImageHeight = characterImage.size.height * characterImageScale
-        characterImage.draw(in: CGRect(x: (characterImageMaxWidth - characterImageWidth) / 2, y: (characterImageMaxHeight - characterImageHeight), width: characterImageWidth, height: characterImageHeight))
-
+        characterImage.draw(in: CGRect(x: (characterImageMaxWidth - characterImageWidth) / 2, y: (characterImageMaxHeight - characterImageHeight) / 2, width: characterImageWidth, height: characterImageHeight))
+        
+        let weaponImageMaxWidth: CGFloat = 152
+        let weaponImageMaxHeight: CGFloat = 180
+        let weaponImageScale = weaponImageMaxWidth / weaponImage.size.width
+        let weaponImageWidth = weaponImage.size.width * weaponImageScale
+        let weaponImageHeight = weaponImage.size.height * weaponImageScale
+        weaponImage.draw(in: CGRect(x: 1418 + (weaponImageMaxWidth - weaponImageWidth) / 2, y: 32 + (weaponImageMaxHeight - weaponImageHeight) / 2, width: weaponImageWidth, height: weaponImageHeight))
+        
+        for (i, artifactImage) in artifactImages.enumerated() {
+            if let artifactImage = artifactImage {
+                let artifactImageMaxWidth: CGFloat = 240
+                let artifactImageMaxHeight: CGFloat = 240
+                let artifactImageScale = min(artifactImageMaxWidth / artifactImage.size.width, artifactImageMaxHeight / artifactImageMaxHeight)
+                let artifactImageWidth = artifactImage.size.width * artifactImageScale
+                let artifactImageHeight = artifactImage.size.height * artifactImageScale
+                let offset = CGFloat(30) + (CGFloat(190) + artifactImageWidth) * CGFloat(i)
+                let rectX = offset + (artifactImageMaxWidth - artifactImageWidth) / 2
+                let rectY = 624 + (artifactImageMaxHeight - artifactImageHeight) / 2
+                let rect = CGRect(x: rectX, y: rectY, width: artifactImageWidth, height: artifactImageHeight)
+                artifactImage.draw(in: rect, blendMode: .sourceAtop, alpha: 0.8)
+            }
+        }
+        
         let buildCardImage = UIGraphicsGetImageFromCurrentImageContext()
 
         //描画を終了
--- GenshinArtifactScorable/Presentation/Screen/BuildCardGenerator/BuildCardGeneratorViewController.swift
+++ GenshinArtifactScorable/Presentation/Screen/BuildCardGenerator/BuildCardGeneratorViewController.swift
@@ -24,38 +24,107 @@ final class BuildCardGeneratorViewController: UIViewController {
     private var scoreCriteria: ScoreCriteria!
     private var imageService: ImageService!
     private var buildCardGenerator: BuildCardGenerator!
+    private var buildCardBaseImage: UIImage!
     
-    private var buildCardBaseImage: UIImage?
     private var characterImage: UIImage?
+    private var weaponImage: UIImage?
+
+    private var artifactImages = [UIImage?](repeating: nil, count: 5)
+    
+    private var isArtifactImageFetchedArray = [Bool](repeating: false, count: 5)
+    private var isArtifactEquipedArray = [Bool](repeating: false, count: 5)
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scoreLabel.text = "スコア：\(String(format: "%.1f", character.calculateTotalScore(criteria: scoreCriteria)))"
+        prepareUIImages()
     }
     
     // MARK: - Private
     
     private func prepareUIImages() {
-        buildCardBaseImage = UIImage(named: "BuildCard/Base/\(character.element.rawValue)")
         imageService.fetchUIImage(imageString: character.imageString)
             .done { characterImage in
                 self.characterImage = characterImage
+                self.imageService.saveUIImage(image: characterImage, imageString: self.character.imageString)
                 self.generateBuildCardIfPrepared()
             }.catch { error in
                 print(error)
             }
+        
+        imageService.fetchUIImage(imageString: character.weapon.iconString)
+            .done { weaponImage in
+                self.weaponImage = weaponImage
+                self.imageService.saveUIImage(image: weaponImage, imageString: self.character.weapon.iconString)
+                self.generateBuildCardIfPrepared()
+            }.catch { error in
+                print(error)
+            }
+    
+        character.artifacts.forEach { artifact in
+            imageService.fetchUIImage(imageString: artifact.iconString)
+                .done { artifactImage in
+                    switch artifact.artifactType {
+                    case .flower:
+                        self.artifactImages[0] = artifactImage
+                        self.isArtifactImageFetchedArray[0] = true
+                    case .plume:
+                        self.artifactImages[1] = artifactImage
+                        self.isArtifactImageFetchedArray[1] = true
+                    case .sands:
+                        self.artifactImages[2] = artifactImage
+                        self.isArtifactImageFetchedArray[2] = true
+                    case .goblet:
+                        self.artifactImages[3] = artifactImage
+                        self.isArtifactImageFetchedArray[3] = true
+                    case .circlet:
+                        self.artifactImages[4] = artifactImage
+                        self.isArtifactImageFetchedArray[4] = true
+                    }
+                    self.imageService.saveUIImage(image: artifactImage, imageString: artifact.iconString)
+                    self.generateBuildCardIfPrepared()
+                }.catch { error in
+                    print(error)
+                    
+                    switch artifact.artifactType {
+                    case .flower:
+                        self.isArtifactImageFetchedArray[0] = true
+                    case .plume:
+                        self.isArtifactImageFetchedArray[1] = true
+                    case .sands:
+                        self.isArtifactImageFetchedArray[2] = true
+                    case .goblet:
+                        self.isArtifactImageFetchedArray[3] = true
+                    case .circlet:
+                        self.isArtifactImageFetchedArray[4] = true
+                    }
+                    self.generateBuildCardIfPrepared()
+                }
+        }
     }
     
     private func generateBuildCardIfPrepared() {
+        guard let characterImage = characterImage, let weaponImage = weaponImage else { return }
         
         // 全ての画像が用意できたら生成する(聖遺物をつけていない場合，聖遺物の画像がnilになるので今のままだとヤバい)
         // 全ての画像についてAPIを叩き終えたかをどう管理するべき？
         // ビルドカード作成をAPIが叩き終わるごとに毎回呼ぶ？その場合，ビルドカード生成コードの引数を(現状で完成しているまでのビルドカード, 新しく追加する画像, 追加する画像のパーツ名(新しく追加する画像をどこに配置するかを判断するため，enumとかで管理する))とかにする．ビルドカード生成中に他のAPIが完了して再度呼ばれるとやばそう
-        if let buildCardBaseImage = buildCardBaseImage, let characterImage = characterImage {
-            buildCardImageView.image = buildCardGenerator.buildCardCreate(buildCardBaseImage: buildCardBaseImage, characterImage: characterImage)
+        if isArtifactsImagesPrepared() {
+            buildCardImageView.image = buildCardGenerator.buildCardCreate(buildCardBaseImage: buildCardBaseImage, characterImage: characterImage, weaponImage: weaponImage, artifactImages: artifactImages)
         }
+    }
+    
+    private func isArtifactsImagesPrepared() -> Bool {
+        var isPrepared = true
+        // 装備しているにも関わらず，画像取得の通信が終わってない場合，ビルドカードの準備ができてない
+        for (i, value) in isArtifactImageFetchedArray.enumerated() {
+            if !value, isArtifactEquipedArray[i] {
+                isPrepared = false
+            }
+        }
+        return isPrepared
     }
 }
 
@@ -65,6 +134,21 @@ extension BuildCardGeneratorViewController: Storyboardable {
         self.scoreCriteria = dependency.scoreCriteria
         self.imageService = ImageService()
         self.buildCardGenerator = BuildCardGenerator()
-        prepareUIImages()
+        self.buildCardBaseImage = UIImage(named: "BuildCard/Base/\(character.element.rawValue)")
+        
+        character.artifacts.forEach { artifact in
+            switch artifact.artifactType {
+            case .flower:
+                self.isArtifactEquipedArray[0] = true
+            case .plume:
+                self.isArtifactEquipedArray[1] = true
+            case .sands:
+                self.isArtifactEquipedArray[2] = true
+            case .goblet:
+                self.isArtifactEquipedArray[3] = true
+            case .circlet:
+                self.isArtifactEquipedArray[4] = true
+            }
+        }
     }
 }
